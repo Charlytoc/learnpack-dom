1cd4e995c6c200c07e03134683ce3e189963ea23
Tomas Gonzalez <gonzalez.tommy2001@gmail.com>
2021-07-27 18:27:54 -0400

    Updated plugin

--- src/compile.js
+++ src/compile.js
@@ -1,5 +1,5 @@
 const fs = require('fs')
-const path = require('path');s
+const path = require('path');
 const webpack = require('webpack');
 const { Utils, CompilationError } = require('learnpack/plugin')
 
